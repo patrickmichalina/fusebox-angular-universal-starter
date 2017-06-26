418e0fef63209200748b43bed39fc53944cda554
Patrick Michalina <patrickmichalina@mac.com>
2017-06-26 13:06:23 -0500

    fix

--- src/server/server.ts
+++ src/server/server.ts
@@ -24,7 +24,7 @@ app.engine('html', ngExpressEngine({
 }));
 app.set('view engine', 'html');
 app.set('views', root);
-app.use(express.static(root, { index: false }))
+app.use(express.static(root, { index: false, maxAge: '1yr' }))
 app.get('/*', (req, res) => {
   return res.render('index', {
     req,
