78fd1a79fb391677bd6b8b62200110ba98077acd
Patrick Michalina <patrickmichalina@mac.com>
2017-06-30 00:32:27 -0500

    chore: heroku ssl

--- src/server/heroku.force-ssl.ts
+++ src/server/heroku.force-ssl.ts
@@ -1,7 +1,10 @@
 import * as express from 'express';
 
 export const forceSsl = function (req: express.Request, res: express.Response, next: express.NextFunction) {
-  if (req.headers['x-forwarded-proto'] !== 'https' || req.headers.host !== process.env.HOST) {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    res.header('Cache-Control', 'no-cache, no-store, must-revalidate');
+    res.header('Pragma', 'no-cache');
+    res.header('Expires', '0');
     return res.redirect(['https://', req.get('Host'), req.url].join(''));
   }
   return next();
