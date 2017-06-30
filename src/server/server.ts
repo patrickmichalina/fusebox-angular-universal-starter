ba64a078ddb3b96321145bf68716e6916b494a64
Patrick Michalina <patrickmichalina@mac.com>
2017-06-29 22:42:06 -0500

    chore: turning off https redirect to test cache

--- src/server/server.ts
+++ src/server/server.ts
@@ -9,7 +9,7 @@ import * as favicon from 'serve-favicon';
 import { join, resolve } from 'path';
 import { ngExpressEngine } from '@nguniversal/express-engine';
 import { AppServerModule } from './app.server.module';
-import { forceSsl } from './heroku.force-ssl';
+// import { forceSsl } from './heroku.force-ssl';
 
 require('ts-node/register');
 
@@ -19,7 +19,7 @@ const settings = require(pkg).config.server;
 const port = process.env.PORT || settings.port;
 const app = express();
 
-if (process.env.HEROKU) app.use(forceSsl);
+// if (process.env.HEROKU) app.use(forceSsl);
 
 app.use(compression());
 app.use(favicon(join(root, 'assets/favicon.ico')));
