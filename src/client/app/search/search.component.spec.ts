399effa76e1c9f526343bf8c60b7bb40d8d6dc86
Patrick Michalina <patrickmichalina@mac.com>
2017-06-29 19:41:54 -0500

    tests: stubbed out service test

--- src/client/app/search/search.component.spec.ts
+++ src/client/app/search/search.component.spec.ts
@@ -1,10 +1,17 @@
+import { Observable } from 'rxjs/Observable';
+import { SearchService, ISearchService } from './search.service';
 import { SearchComponent } from './search.component';
 import { async, TestBed } from '@angular/core/testing';
+import { URLSearchParams } from '@angular/http';
+import '../../operators';
 
 describe(SearchComponent.name, () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      providers: [
+        { provide: SearchService, useValue: new MockSearchService() }
+      ],
       declarations: [SearchComponent]
     });
   });
@@ -19,3 +26,20 @@ describe(SearchComponent.name, () => {
         });
     }));
 });
+
+class MockSearchService implements ISearchService {
+  public returnValue: any[];
+
+  search(search?: string | undefined, sort?: string | undefined, order?: string | undefined): Observable<any> {
+    const params = new URLSearchParams();
+    
+    if(search) params.set('q', search);
+    if(sort) params.set('sort', sort);
+    if(order) params.set('order', order);
+    
+    return Observable.create((observer: any) => {
+      observer.next(this.returnValue);
+      observer.complete();
+    });
+  }
+}
--- src/client/app/search/search.service.spec.ts
+++ src/client/app/search/search.service.spec.ts
@@ -1,21 +1,43 @@
-import { SearchService } from './search.service';
-// import { async, TestBed } from '@angular/core/testing';
+import { TransferState } from '../modules/transfer-state/transfer-state';
+import { TransferHttp } from '../modules/transfer-http/transfer-http';
+import { SearchService, ISearchService } from './search.service';
+import { async, TestBed } from '@angular/core/testing';
+import { MockBackend } from '@angular/http/testing';
+import { BaseRequestOptions, ConnectionBackend, Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import '../../operators';
 
 describe(SearchService.name, () => {
 
-  // beforeEach(() => {
-  //   TestBed.configureTestingModule({
-  //     declarations: [SearchComponent]
-  //   });
-  // });
+  beforeEach(() => {
 
-  // it('should compile',
-  //   async(() => {
-  //     TestBed
-  //       .compileComponents()
-  //       .then(() => {
-  //         const fixture = TestBed.createComponent(SearchComponent);
-  //         expect(fixture.componentInstance).not.toBeNull();
-  //       });
-  //   }));
+    TestBed.configureTestingModule({
+      providers: [
+        SearchService,
+        MockBackend,
+        BaseRequestOptions,
+        TransferState,
+        {
+          provide: Http,
+          useFactory: (backend: ConnectionBackend, options: BaseRequestOptions) => new Http(backend, options),
+          deps: [MockBackend, BaseRequestOptions]
+        },
+        {
+          provide: TransferHttp,
+          useFactory: (http: Http, transferState: TransferState) => new TransferHttp(http, transferState),
+          deps: [Http, TransferState]
+        }
+      ]
+    });
+  });
+
+  it('should compile', async(() => {
+    const service = TestBed.get(SearchService) as ISearchService;
+    expect(service).not.toBeNull();
+  }));
+
+  it('should return an Observable when search is called', async(() => {
+    const service = TestBed.get(SearchService) as ISearchService;
+    expect(service.search()).toEqual(jasmine.any(Observable));
+  }));
 });
