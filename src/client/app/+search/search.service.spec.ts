a80c87581913222f45d5856d580a8c8bd523f494
Patrick Michalina <patrickmichalina@mac.com>
2017-07-18 08:09:24 -0500

    test: HttpClientTestingModule added to support new HttpClient

--- src/client/app/+search/search.service.spec.ts
+++ src/client/app/+search/search.service.spec.ts
@@ -1,51 +1,32 @@
-// import { TransferState } from '../shared/transfer-state/transfer-state';
-// // import { TransferHttp } from '../shared/transfer-http/transfer-http';
-// import { ISearchService, SearchService } from './search.service';
-// import { async, TestBed } from '@angular/core/testing';
-// import { MockBackend } from '@angular/http/testing';
-// import { BaseRequestOptions, ConnectionBackend, Http } from '@angular/http';
-// import { Observable } from 'rxjs/Observable';
-// import '../../operators';
+import { TransferState } from '../shared/transfer-state/transfer-state';
+import { ISearchService, SearchService } from './search.service';
+import { async, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Observable } from 'rxjs/Observable';
+import '../../operators';
 
-// describe(SearchService.name, () => {
-//   let service: ISearchService;
+describe(SearchService.name, () => {
+  let service: ISearchService;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       providers: [
-//         SearchService,
-//         MockBackend,
-//         BaseRequestOptions,
-//         TransferState,
-//         {
-//           provide: Http,
-//           useFactory: (backend: ConnectionBackend, options: BaseRequestOptions) => new Http(backend, options),
-//           deps: [MockBackend, BaseRequestOptions]
-//         },
-//         // {
-//         //   provide: TransferHttp,
-//         //   useFactory: (http: Http, transferState: TransferState) => new TransferHttp(http, transferState),
-//         //   deps: [Http, TransferState]
-//         // }
-//       ]
-//     }).compileComponents();
-//   }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        SearchService,
+        TransferState
+      ]
+    }).compileComponents();
+  }));
 
-//   beforeEach(() => {
-//     service = TestBed.get(SearchService);
-//   });
+  beforeEach(() => {
+    service = TestBed.get(SearchService);
+  });
 
-//   it('should compile', async(() => {
-//     expect(service).not.toBeNull();
-//   }));
+  it('should compile', async(() => {
+    expect(service).not.toBeNull();
+  }));
 
-//   it('should return an Observable when search is called', async(() => {
-//     expect(service.search()).toEqual(expect.any(Observable));
-//   }));
-// });
-
-describe('temp', () => {
-    it('should compile', () => {
-      expect(1).toBe(1);
-    });
+  it('should return an Observable when search is called', async(() => {
+    expect(service.search()).toEqual(expect.any(Observable));
+  }));
 });
--- src/client/app/app.component.spec.ts
+++ src/client/app/app.component.spec.ts
@@ -1,79 +1,73 @@
-// import { AppComponent } from './app.component';
-// import { EnvConfig } from '../../../tools/config/app.config';
-// import { TestBed } from '@angular/core/testing';
-// import { APP_BASE_HREF } from '@angular/common';
-// import { async } from '@angular/core/testing';
-// import { Route } from '@angular/router';
-// import { RouterTestingModule } from '@angular/router/testing';
-// import { Component } from '@angular/core';
-// import { HomeComponent } from './+home/home.component';
-// import { AboutComponent } from './+about/about.component';
-// import { SearchComponent } from './+search/search.component';
-// import { NavbarComponent } from './shared/navbar/navbar.component';
-// import { ENV_CONFIG } from './app.config';
-// import { EnvironmentService } from './shared/services/environment.service';
-// import { Angulartics2GoogleAnalytics, Angulartics2Module } from 'angulartics2';
-// import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { EnvConfig } from '../../../tools/config/app.config';
+import { TestBed } from '@angular/core/testing';
+import { APP_BASE_HREF } from '@angular/common';
+import { async } from '@angular/core/testing';
+import { Route } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Component } from '@angular/core';
+import { HomeComponent } from './+home/home.component';
+import { AboutComponent } from './+about/about.component';
+import { SearchComponent } from './+search/search.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { ENV_CONFIG } from './app.config';
+import { EnvironmentService } from './shared/services/environment.service';
+import { Angulartics2GoogleAnalytics, Angulartics2Module } from 'angulartics2';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
-// const TESTING_CONFIG: EnvConfig = {
-//   name: 'Fusebox Angular Universal Starter',
-//   // tslint:disable-next-line:max-line-length
-//   description: 'Seed project for Angular Universal apps featuring Server-Side Rendering (SSR), FuseBox, dev/prod builds, Brotli/Gzip, SCSS, favicon generation, @types, unit testing w/ Jest, and sitemap generator. Created by Patrick Michalina',
-//   pageTitleSeparator: ' - ',
-//   og: {
-//     defaultSocialImage: 'https://d3anl5a3ibkrdg.cloudfront.net/assets/favicons/android-chrome-512x512',
-//     facebookAppId: '117309532219749'
-//   },
-//   server: {
-//     host: 'http://localhost',
-//     port: 8083,
-//     minifyIndex: true
-//   }
-// };
+const TESTING_CONFIG: EnvConfig = {
+  name: 'Fusebox Angular Universal Starter',
+  // tslint:disable-next-line:max-line-length
+  description: 'Seed project for Angular Universal apps featuring Server-Side Rendering (SSR), FuseBox, dev/prod builds, Brotli/Gzip, SCSS, favicon generation, @types, unit testing w/ Jest, and sitemap generator. Created by Patrick Michalina',
+  pageTitleSeparator: ' - ',
+  og: {
+    defaultSocialImage: 'https://d3anl5a3ibkrdg.cloudfront.net/assets/favicons/android-chrome-512x512',
+    facebookAppId: '117309532219749'
+  },
+  server: {
+    host: 'http://localhost',
+    port: 8083,
+    minifyIndex: true
+  }
+};
 
-// describe('App component', () => {
-//   const config: Array<Route> = [
-//     { path: '', component: HomeComponent },
-//     { path: 'about', component: AboutComponent },
-//     { path: 'search', component: SearchComponent }
-//   ];
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       imports: [
-//         HttpClientModule,
-//         RouterTestingModule.withRoutes(config),
-//         Angulartics2Module.forRoot([ Angulartics2GoogleAnalytics ])],
-//       declarations: [TestComponent, NavbarComponent, AppComponent,
-//         HomeComponent, AboutComponent, SearchComponent],
-//       providers: [
-//         { provide: APP_BASE_HREF, useValue: '/' },
-//         { provide: ENV_CONFIG, useValue: TESTING_CONFIG },
-//         EnvironmentService
-//       ]
-//     }).compileComponents();
-//   }));
+describe('App component', () => {
+  const config: Array<Route> = [
+    { path: '', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'search', component: SearchComponent }
+  ];
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule.withRoutes(config),
+        Angulartics2Module.forRoot([ Angulartics2GoogleAnalytics ])],
+      declarations: [TestComponent, NavbarComponent, AppComponent,
+        HomeComponent, AboutComponent, SearchComponent],
+      providers: [
+        { provide: APP_BASE_HREF, useValue: '/' },
+        { provide: ENV_CONFIG, useValue: TESTING_CONFIG },
+        EnvironmentService
+      ]
+    }).compileComponents();
+  }));
 
-//   it('should build without a problem',
-//     async(() => {
-//       TestBed
-//         .compileComponents()
-//         .then(() => {
-//           const fixture = TestBed.createComponent(TestComponent);
-//           const compiled = fixture.nativeElement;
+  it('should build without a problem',
+    async(() => {
+      TestBed
+        .compileComponents()
+        .then(() => {
+          const fixture = TestBed.createComponent(TestComponent);
+          const compiled = fixture.nativeElement;
 
-//           expect(compiled).toBeTruthy();
-//         });
-//     }));
-// });
-
-// @Component({
-//   selector: 'test-cmp',
-//   template: '<pm-app></pm-app>'
-// })
-// class TestComponent { }
-
-describe('temp', () => {
-    it('should compile', () => {
-      expect(1).toBe(1);
-    });
+          expect(compiled).toBeTruthy();
+        });
+    }));
 });
+
+@Component({
+  selector: 'test-cmp',
+  template: '<pm-app></pm-app>'
+})
+class TestComponent { }
