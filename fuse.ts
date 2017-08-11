9d1796b062811470e202e6da5dafb743ac9d8f48
Patrick Michalina <patrickmichalina@mac.com>
2017-08-10 19:42:54 -0500

    refactor(tools): split fusebox server and client configurations (#60)

--- fuse.ts
+++ fuse.ts
@@ -2,7 +2,6 @@ import { Ng2TemplatePlugin } from 'ng2-fused';
 import { argv } from 'yargs';
 import { BUILD_CONFIG, ENV_CONFIG_INSTANCE, isProdBuild, cachebuster, cdn } from './tools/config/build.config';
 import { NgLazyPlugin } from './tools/plugins/ng-lazy';
-import { Plugin } from 'fuse-box/src/core/WorkflowContext';
 import {
   EnvPlugin,
   FuseBox,
@@ -18,6 +17,7 @@ import './tools/tasks';
 
 const isAot = argv.aot;
 const isBuildServer = argv.ci;
+const isSpaOnly = argv.spa;
 const baseEntry = isAot ? 'main.aot' : 'main';
 const mainEntryFileName = isProdBuild ? `${baseEntry}-prod` : `${baseEntry}`;
 const appBundleName = isProdBuild ? `js/app-${cachebuster}` : `js/app`;
@@ -26,18 +26,12 @@ const vendorBundleInstructions = ` ~ client/${mainEntryFileName}.ts`;
 const serverBundleInstructions = ' > [server/server.ts]';
 const appBundleInstructions = ` !> [client/${mainEntryFileName}.ts]`;
 
-const options: any = {
+const baseOptions = {
   homeDir: './src',
   output: `${BUILD_CONFIG.outputDir}/$name.js`,
-  experimentalFeatures: false,
-  sourceMaps: isProdBuild || process.env.CI
-    ? { project: false, vendor: false, inline: false }
-    : { project: true, vendor: true, inline: true },
-  target: 'browser',
   cache: false,
+  target: 'browser',
   plugins: [
-    EnvPlugin(ENV_CONFIG_INSTANCE), // Leave this as first plugin
-    isProdBuild && UglifyESPlugin(),
     NgLazyPlugin({
       cdn,
       angularAppEntry: '',
@@ -51,40 +45,67 @@ const options: any = {
       SassPlugin({ indentedSyntax: false, importer: true, sourceMap: false, outputStyle: 'compressed' } as any), RawPlugin()],
     JSONPlugin(),
     HTMLPlugin({ useDefault: false }),
+  ]
+}
 
-  ] as Plugin[]
-};
+const appOptions = {
+  ...baseOptions,
+  sourceMaps: isProdBuild || process.env.CI
+    ? { project: false, vendor: false, inline: false }
+    : { project: true, vendor: true, inline: true },
+  plugins: [
+    EnvPlugin(ENV_CONFIG_INSTANCE),
+    isProdBuild && UglifyESPlugin(),
+    ...baseOptions.plugins
+  ]
+}
 
-Sparky.task('build', () => {
-  const fuse = FuseBox.init(options as any);
+const serverOptions = {
+  ...baseOptions,
+  sourceMaps: false,
+  plugins: [
+    EnvPlugin(ENV_CONFIG_INSTANCE),
+    ...baseOptions.plugins
+  ]
+}
+
+Sparky.task('build.server', () => {
+  if (isSpaOnly) return Promise.resolve();
+
+  const fuse = FuseBox.init(serverOptions as any);
+  const serverBundle = fuse.bundle('server').instructions(serverBundleInstructions);
+
+  if (!isBuildServer && !argv['build-only']) {
+    serverBundle.completed(proc => {
+      if (cdn) removeCdn(proc, cdn);
+      proc.start()
+    }).watch();
+  } else {
+    serverBundle.completed(proc => {
+      if (cdn) removeCdn(proc, cdn);
+    })
+  }
+
+  return fuse.run();
+});
+
+Sparky.task('build.app', () => {
+  const fuse = FuseBox.init(appOptions as any);
   const path = isAot ? 'client/.aot/src/client/app' : 'client/app';
   const vendorBundle = fuse.bundle(`${vendorBundleName}`).instructions(vendorBundleInstructions);
   const appBundle = fuse.bundle(appBundleName)
     .instructions(`${appBundleInstructions} + [${path}/**/!(*.spec|*.e2e-spec|*.ngsummary|*.snap).*]`)
     .plugin([EnvPlugin(ENV_CONFIG_INSTANCE)]);
-  const serverBundle = fuse.bundle('server')
-    .sourceMaps({ project: false, vendor: false, inline: false })
-    .instructions(serverBundleInstructions);
 
   if (!isBuildServer && !argv['build-only']) {
     vendorBundle.watch();
     appBundle.watch()
 
     if (argv.spa) {
-      fuse.dev({ port: ENV_CONFIG_INSTANCE.server.port, root: 'dist', open : true });
+      fuse.dev({ port: ENV_CONFIG_INSTANCE.server.port, root: 'dist', open: true });
       vendorBundle.hmr();
       appBundle.hmr();
-    } else {
-      serverBundle.completed(proc => {
-        if (cdn) removeCdn(proc, cdn);
-
-        proc.start()
-      }).watch();
     }
-  } else {
-    serverBundle.completed(proc => {
-      if (cdn) removeCdn(proc, cdn);
-    })
   }
 
   return fuse.run();
@@ -99,7 +120,8 @@ Sparky.task('serve', () => {
     .then(() => isProdBuild || !BUILD_CONFIG.skipFaviconGenerationOnDev ? Sparky.start('favicons') : Promise.resolve())
     .then(() => Sparky.start('sass'))
     .then(() => Sparky.start('sass.files'))
-    .then(() => Sparky.start('build'))
+    .then(() => Sparky.start('build.app'))
+    .then(() => Sparky.start('build.server'))
     .then(() => Sparky.start('js.files'))
     .then(() => Sparky.start('index.inject'))
     .then(() => Sparky.start('index.minify'))
