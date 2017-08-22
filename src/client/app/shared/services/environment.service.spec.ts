3db85f5a83f7474574c366c65180282a61446d16
Patrick Michalina <patrickmichalina@mac.com>
2017-08-21 22:23:01 -0500

    test: improved coverage of state transfer services (#92)

--- src/client/app/shared/services/environment.service.spec.ts
+++ src/client/app/shared/services/environment.service.spec.ts
@@ -1,6 +1,6 @@
 import { EnvironmentService, IEnvironmentService } from './environment.service';
 import { async, TestBed } from '@angular/core/testing';
-import { ENV_CONFIG, FuseBoxEnvConfig } from '../../app.config';
+import { ENV_CONFIG } from '../../app.config';
 
 describe(EnvironmentService.name, () => {
   let service: IEnvironmentService;
@@ -9,7 +9,7 @@ describe(EnvironmentService.name, () => {
     TestBed.configureTestingModule({
       providers: [
         EnvironmentService,
-        { provide: ENV_CONFIG, useValue: FuseBoxEnvConfig }
+        { provide: ENV_CONFIG, useValue: { someValue: 1 } }
       ]
     }).compileComponents();
   }));
@@ -18,7 +18,17 @@ describe(EnvironmentService.name, () => {
     service = TestBed.get(EnvironmentService);
   });
 
+  afterEach(() => {
+    TestBed.resetTestingModule();
+  });
+
   it('should construct', async(() => {
-    expect(service).not.toBeNull();
+    expect(service).toBeDefined();
+  }));
+
+  it('should return config', async(() => {
+    expect(service.config).toEqual({
+      someValue: 1
+    });
   }));
 });
--- src/client/app/shared/transfer-state/transfer-state.ts
+++ src/client/app/shared/transfer-state/transfer-state.ts
@@ -1,7 +1,18 @@
 import { Injectable } from '@angular/core';
 
+export interface ITransferState {
+  get(key: string): any;
+  keys(): IterableIterator<string>;
+  set(key: string, value: any): Map<string, any>;
+  toJson(): any;
+  initialize(obj: { [key: string]: any }): Map<string, any>;
+  bust(): Map<string, any>;
+  bustByKey(key: string): Map<string, any>;
+  bustByKeyPattern(pattern: RegExp): Map<string, any>;
+}
+
 @Injectable()
-export class TransferState {
+export class TransferState implements ITransferState {
   private _map = new Map<string, any>();
 
   keys() {
@@ -16,6 +27,23 @@ export class TransferState {
     return this._map.set(key, value);
   }
 
+  bust(): Map<string, any> {
+    this._map = new Map<string, any>();
+    return this._map;
+  }
+
+  bustByKey(key: string): Map<string, any> {
+    this._map.delete(key);
+    return this._map;
+  }
+
+  bustByKeyPattern(pattern: RegExp): Map<string, any> {
+    Array.from(this.keys())
+      .filter(a => pattern.test(a))
+      .forEach(key =>  this._map.delete(key));
+    return this._map;
+  }
+
   toJson(): any {
     const obj: any = {};
     Array.from(this.keys())
@@ -25,13 +53,11 @@ export class TransferState {
     return obj;
   }
 
-  initialize(obj: any): void {
-    Object.keys(obj)
-      .forEach(key => {
-        this.set(key, obj[key]);
-      });
+  initialize(obj: { [key: string]: any }): Map<string, any> {
+    Object.keys(obj).forEach(key => this.set(key, obj[key]));
+    return this._map;
   }
 
-  // tslint:disable-next-line:no-empty
+  // tslint:disable:no-empty
   inject(): void { }
 }
