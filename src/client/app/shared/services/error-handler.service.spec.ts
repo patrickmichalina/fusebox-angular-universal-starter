143a6667616450d1a1847bb6c8ed3e7502f94fe5
Patrick Michalina <patrickmichalina@mac.com>
2017-08-24 09:43:34 -0500

    fixes

--- src/client/app/shared/services/error-handler.service.spec.ts
+++ src/client/app/shared/services/error-handler.service.spec.ts
@@ -1,13 +1,20 @@
+import { RouterTestingModule } from '@angular/router/testing';
+import { PlatformService } from './platform.service';
 import { GlobalErrorHandler } from './error-handler.service';
-import { async, TestBed } from '@angular/core/testing';
+import { async, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { LoggingService } from './logging.service';
+import * as StackTrace from 'stacktrace-js';
 
 describe(GlobalErrorHandler.name, () => {
   let service: GlobalErrorHandler;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       providers: [
-        GlobalErrorHandler
+        GlobalErrorHandler,
+        PlatformService,
+        { provide: LoggingService, useValue: new MockLoggingService() }
       ]
     }).compileComponents();
   }));
@@ -23,4 +30,31 @@ describe(GlobalErrorHandler.name, () => {
   it('should construct', async(() => {
     expect(service).toBeDefined();
   }));
+
+  it('should log an error', fakeAsync(() => {
+    const spy = jest.spyOn(TestBed.get(LoggingService), 'error');
+    service.handleError(new Error('Testing this error'));
+    tick();
+    expect(spy).toHaveBeenCalledTimes(1);
+  }));
+
+  it('should log an error with message, url, and stack trace', fakeAsync(() => {
+    const spy = jest.spyOn(TestBed.get(LoggingService), 'error');
+    const error = new Error('Testing this error');
+    service.handleError(error);
+    tick();
+    StackTrace.fromError(error).then(stackframes => {
+      const stack = stackframes
+        .splice(0, 20)
+        .map(sf => sf.toString())
+        .join('\n');
+        expect(spy).toBeCalledWith({ message: 'Testing this error', url: '', stack });
+    });
+  }));
 });
+
+class MockLoggingService {
+  error(...msg: any[]) {
+    return msg;
+  }
+}
--- src/client/app/shared/services/logging.service.spec.ts
+++ src/client/app/shared/services/logging.service.spec.ts
@@ -1,28 +1,31 @@
 import { PlatformService } from './platform.service';
 import { ILoggingService, LoggingService } from './logging.service';
 import { async, TestBed } from '@angular/core/testing';
+import { PLATFORM_ID } from '@angular/core';
 
 describe(LoggingService.name, () => {
-  let service: ILoggingService;
+  describe('browser', () => {
+    let service: ILoggingService;
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        providers: [
+          LoggingService,
+          PlatformService,
+          { provide: PLATFORM_ID, useValue: 'browser' }
+        ]
+      }).compileComponents();
+    }));
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      providers: [
-        LoggingService,
-        PlatformService
-      ]
-    }).compileComponents();
-  }));
+    beforeEach(async(() => {
+      service = TestBed.get(LoggingService);
+    }));
 
-  beforeEach(async(() => {
-    service = TestBed.get(LoggingService);
-  }));
+    afterEach(async(() => {
+      TestBed.resetTestingModule();
+    }));
 
-  afterEach(async(() => {
-    TestBed.resetTestingModule();
-  }));
-
-  it('should construct', async(() => {
-    expect(service).toBeDefined();
-  }));
+    it('should construct', async(() => {
+      expect(service).toBeDefined();
+    }));
+  });
 });
