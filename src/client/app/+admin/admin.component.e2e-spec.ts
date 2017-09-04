113a7d1eae63246837f8978366ff3d30c509a837
Patrick Michalina <patrickmichalina@mac.com>
2017-09-04 12:09:56 -0500

    test: update jest to v21.0.0

--- src/client/app/+admin/admin.component.e2e-spec.ts
+++ src/client/app/+admin/admin.component.e2e-spec.ts
@@ -1,7 +1,6 @@
 import { baseUrl, browser } from '../../../../tools/test/jest.e2e-setup'
 
 describe('Admin Page', () => {
-
   test('should have title', async () => {
     const page = browser.goto(`${baseUrl}/admin`)
 
@@ -10,5 +9,4 @@ describe('Admin Page', () => {
 
     expect(text).toEqual('Admin - Fusebox Angular Universal Starter')
   })
-
 })
--- tools/test/jest.mocks.ts
+++ tools/test/jest.mocks.ts
@@ -7,12 +7,9 @@ const mock = () => {
     clear: () => storage = {},
   };
 };
-Object.defineProperty(window, 'CSS', {value: mock()});
+Object.defineProperty(window, 'CSS', { value: mock() });
 Object.defineProperty(window, 'localStorage', { value: mock() });
 Object.defineProperty(window, 'sessionStorage', { value: mock() });
-Object.defineProperty(document, 'doctype', {
-  value: '<!DOCTYPE html>'
-});
 Object.defineProperty(window, 'getComputedStyle', {
   value: () => {
     return {
