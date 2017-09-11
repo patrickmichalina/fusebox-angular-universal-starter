c9b3ae8fbfc82db43d9258b6eab462a05be08729
Patrick Michalina <patrickmichalina@mac.com>
2017-09-11 00:19:26 -0500

    feat: send error to google analytics (#155)

--- src/client/app/shared/shared.module.ts
+++ src/client/app/shared/shared.module.ts
@@ -1,14 +1,14 @@
+import { EnvironmentService } from './services/environment.service'
 import { ModuleWithProviders, NgModule } from '@angular/core'
-import { PlatformService } from './services/platform.service'
 import { RouterModule } from '@angular/router'
 import { NavbarComponent } from './navbar/navbar.component'
 import { CookieService } from './services/cookie.service'
 import { CommonModule } from '@angular/common'
-import { EnvironmentService } from './services/environment.service'
+import { PlatformService } from './services/platform.service'
 import { LOGGER_CONFIG, LoggingService } from './services/logging.service'
 import { ENV_CONFIG } from '../app.config'
 import { NavbarService } from './navbar/navbar.service'
-import { Angulartics2Module } from 'angulartics2'
+import { Angulartics2GoogleAnalytics, Angulartics2Module } from 'angulartics2'
 import { MdButtonModule, MdCardModule } from '@angular/material'
 import { FlexLayoutModule } from '@angular/flex-layout'
 
@@ -16,6 +16,31 @@ declare var __process_env__: any
 
 export function fuseBoxConfigFactory() {
   return __process_env__
+}
+
+export function loggerConfigFactory(ps: PlatformService, gooogleAnalytics: Angulartics2GoogleAnalytics) {
+  return {
+    name: 'Angular Universal App',
+    type: 'app',
+    streams: [{
+      level: 'error',
+      stream: {
+        write: (err: any) => {
+          if (ps.isBrowser) {
+            console.error('Application error', err)
+            if ((window as any).ga) {
+              gooogleAnalytics.exceptionTrack(err)
+            } else {
+              console.log('(Application error was not logged to analytics provider)')
+            }
+          } else {
+            console.error(err)
+          }
+        }
+      },
+      type: 'raw'
+    }]
+  }
 }
 
 @NgModule({
@@ -41,10 +66,8 @@ export function fuseBoxConfigFactory() {
     { provide: ENV_CONFIG, useFactory: fuseBoxConfigFactory },
     {
       provide: LOGGER_CONFIG,
-      useValue: {
-        name: 'Angular Universal App',
-        type: 'app'
-      }
+      useFactory: loggerConfigFactory,
+      deps: [PlatformService, Angulartics2GoogleAnalytics]
     },
     PlatformService,
     CookieService,
