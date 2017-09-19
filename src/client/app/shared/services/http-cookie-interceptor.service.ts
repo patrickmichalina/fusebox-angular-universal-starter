460051623afd11a0a5c5c16a8ed25bc439d3ee19
Patrick Michalina <patrickmichalina@mac.com>
2017-09-19 12:27:33 -0500

    feat: add universal cookie request support (#182)

---
+++ src/client/app/shared/services/http-cookie-interceptor.service.ts
@@ -0,0 +1,41 @@
+import { REQUEST } from '@nguniversal/express-engine/src/tokens'
+import { PlatformService } from './platform.service'
+import { Observable } from 'rxjs/Observable'
+import { Injectable, InjectionToken, Injector } from '@angular/core'
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http'
+import * as express from 'express'
+
+// tslint:disable-next-line:no-require-imports
+const URL = require('url-parse')
+
+export const COOKIE_HOST_WHITELIST = new InjectionToken<string[]>('app.cookie.whitelist')
+
+@Injectable()
+export class HttpCookieInterceptor implements HttpInterceptor {
+
+  constructor(private ps: PlatformService, private injector: Injector) { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const whitelistedDomains = this.injector.get(COOKIE_HOST_WHITELIST) as string[]
+    const url = new URL(req.url)
+
+    if (!whitelistedDomains.some(a => a === url.hostname)) return next.handle(req)
+
+    if (this.ps.isServer) {
+      const serverRequest = this.injector.get(REQUEST) as express.Request
+
+      const cookieString = Object.keys(serverRequest.cookies || {}).reduce((accumulator, cookieName) => {
+        accumulator += `${cookieName}=${serverRequest.cookies[cookieName]};`
+        return accumulator
+      }, '')
+
+      return next.handle(req.clone({
+        withCredentials: true,
+        headers: req.headers.set('Cookie', cookieString)
+      }))
+    }
+    return next.handle(req.clone({
+      withCredentials: true
+    }))
+  }
+}
