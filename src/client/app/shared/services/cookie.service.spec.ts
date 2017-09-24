ae059e48c51f690c746b7917cd1e3e87a68ffbf0
Patrick Michalina <patrickmichalina@mac.com>
2017-09-24 17:42:57 -0500

    chore: updated dependencies (#196)

--- src/client/app/shared/services/cookie.service.spec.ts
+++ src/client/app/shared/services/cookie.service.spec.ts
@@ -1,7 +1,7 @@
 import { PlatformService } from './platform.service'
 import { CookieService, ICookieService } from './cookie.service'
 import { async, TestBed } from '@angular/core/testing'
-import { REQUEST } from '@nguniversal/express-engine/modules/express-engine/tokens'
+import { REQUEST } from '@nguniversal/express-engine/tokens'
 
 describe(CookieService.name, () => {
   let service: ICookieService
