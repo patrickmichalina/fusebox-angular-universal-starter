2e9bb13d3485c930c69518942211b7ae4657a642
Patrick Michalina <patrickmichalina@mac.com>
2017-09-25 18:31:31 -0500

    fixes

--- tools/tasks/seed/clean.ts
+++ tools/tasks/seed/clean.ts
@@ -4,5 +4,6 @@ import { BUILD_CONFIG, taskName } from '../../config/build.config'
 Sparky.task(taskName(__filename), () =>
   Sparky.src(`${BUILD_CONFIG.outputDir}`)
     .clean(`${BUILD_CONFIG.outputDir}`)
+    .clean('.fusebox')
     .clean('.ngc')
     .clean('src/client/.aot'))
