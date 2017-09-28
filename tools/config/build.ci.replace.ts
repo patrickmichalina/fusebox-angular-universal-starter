a881b936b86feac759f787223545721c3ed25177
Patrick Michalina <patrickmichalina@mac.com>
2017-09-27 21:40:41 -0500

    env

--- tools/config/build.ci.replace.ts
+++ tools/config/build.ci.replace.ts
@@ -1,10 +1,10 @@
-// use this to replace values in your env/configs using environment variables from your CI/deployment system
 import { argv } from 'yargs'
 
-export const OVERRIDES = argv['ci-vars']
-  ? {
+// use this to replace values in your env/configs using environment variables from your CI/deployment system
+export const OVERRIDES = argv['ci-vars'] ?
+  {
+    host: `https://${process.env.HEROKU_APP_NAME}.herokuapp.com`,
     endpoints: {
-      api: `https://${process.env.HEROKU_APP_NAME}.herokuapp.com`
+      api: `https://${process.env.HEROKU_APP_NAME}.herokuapp.com/api`
     }
-  }
-  : {}
+  } : {}
