c3d6c332b7dbfdb2e4f8d2cbeba0747f05581b57
Patrick Michalina <patrickmichalina@mac.com>
2017-09-28 21:17:55 -0500

    ssl

--- tools/config/build.ci.replace.ts
+++ tools/config/build.ci.replace.ts
@@ -6,6 +6,6 @@ export const OVERRIDES = argv['ci-vars'] ?
     host: `https://${process.env.HEROKU_APP_NAME}.herokuapp.com`,
     endpoints: {
       api: `https://${process.env.HEROKU_APP_NAME}.herokuapp.com/api`,
-      websocketServer: `ws://${process.env.HEROKU_APP_NAME}.herokuapp.com`
+      websocketServer: `wss://${process.env.HEROKU_APP_NAME}.herokuapp.com`
     }
   } : {}
