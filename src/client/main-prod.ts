09b73c6caaf3a13beb5b95d4872adf02f3c56134
Patrick Michalina <patrickmichalina@mac.com>
2017-09-30 08:38:35 -0500

    fix: add service worker to prod file

--- src/client/main-prod.ts
+++ src/client/main-prod.ts
@@ -9,3 +9,21 @@ import { AppBrowserModule } from './app/app.browser.module'
 enableProdMode()
 
 platformBrowserDynamic().bootstrapModule(AppBrowserModule)
+  .then(() => {
+    registerServiceWorker('ngsw-worker')
+  })
+
+function registerServiceWorker(swName: string) {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register(`/js/${swName}.js`)
+      .then(reg => {
+        console.log('[App] Successful service worker registration', reg)
+      })
+      .catch(err =>
+        console.error('[App] Service worker registration failed', err)
+      )
+  } else {
+    console.error('[App] Service Worker API is not supported in current browser')
+  }
+}
--- src/server/server.angular.module.ts
+++ src/server/server.angular.module.ts
@@ -1,8 +1,11 @@
+import { REQUEST } from '@nguniversal/express-engine/tokens'
 import { AppComponent } from './../client/app/app.component'
-import { enableProdMode, NgModule } from '@angular/core'
+import { APP_BOOTSTRAP_LISTENER, ApplicationRef, enableProdMode, NgModule } from '@angular/core'
 import { ServerModule, ServerTransferStateModule } from '@angular/platform-server'
-import { AppModule } from './../client/app/app.module'
+import { AppModule, REQ_KEY } from './../client/app/app.module'
 import { EnvConfig } from '../../tools/config/app.config'
+import { TransferState } from '@angular/platform-browser'
+import * as express from 'express'
 import 'rxjs/add/operator/filter'
 import 'rxjs/add/operator/first'
 import '../client/operators'
@@ -11,11 +14,37 @@ declare var __process_env__: EnvConfig
 
 if (__process_env__.env !== 'prod') enableProdMode()
 
+export function onBootstrap(appRef: ApplicationRef, transferState: TransferState, req: express.Request) {
+  return () => {
+    appRef.isStable
+      .filter(stable => stable)
+      .first()
+      .subscribe(() => {
+        transferState.set<any>(REQ_KEY, {
+          hostname: req.hostname,
+          originalUrl: req.originalUrl
+        })
+      })
+  }
+}
+
 @NgModule({
   imports: [
     ServerModule,
     ServerTransferStateModule,
     AppModule
+  ],
+  providers: [
+    {
+      provide: APP_BOOTSTRAP_LISTENER,
+      useFactory: onBootstrap,
+      multi: true,
+      deps: [
+        ApplicationRef,
+        TransferState,
+        REQUEST
+      ]
+    }
   ],
   bootstrap: [AppComponent]
 })
