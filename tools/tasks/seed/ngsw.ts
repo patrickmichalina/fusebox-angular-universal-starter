d28952a4db68fb2c57b91430c94fe682d5c32ee1
Patrick Michalina <patrickmichalina@mac.com>
2017-10-06 08:56:00 -0500

    build: minify ngsw-worker.js (#225)

---
+++ tools/tasks/seed/ngsw.ts
@@ -0,0 +1,5 @@
+import { Sparky } from 'fuse-box'
+import { taskName } from '../../config/build.config'
+
+Sparky.task(taskName(__filename), ['ngsw-worker', 'ngsw-worker.min', 'ngsw-json'], () => {})
+
