19737759defa833b24272ce8c33cf6cfbc5dff2d
Patrick Michalina <patrickmichalina@mac.com>
2017-10-08 14:27:09 -0500

    wip

--- src/client/app/shared/login-card/login-card.component.ts
+++ src/client/app/shared/login-card/login-card.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core'
 import { AngularFireAuth } from 'angularfire2/auth'
 import * as firebase from 'firebase/app'
 import { FormControl, FormGroup, Validators } from '@angular/forms'
@@ -12,32 +12,46 @@ const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LoginCardComponent {
-  constructor(public afAuth: AngularFireAuth) { }
+  constructor(public afAuth: AngularFireAuth, private cd: ChangeDetectorRef) { }
+  public socialNetworkErr: string
 
   login(provider: string) {
     switch (provider) {
-      case 'facebook': this.afAuth.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider())
+      case 'facebook': this.afAuth.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider()).catch(err => this.socialNetworkError(err))
         break
-      case 'google': this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
+      case 'google': this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).catch(err => this.socialNetworkError(err))
         break
-      case 'github': this.afAuth.auth.signInWithPopup(new firebase.auth.GithubAuthProvider())
+      case 'github': this.afAuth.auth.signInWithPopup(new firebase.auth.GithubAuthProvider()).catch(err => this.socialNetworkError(err))
         break
-      case 'twitter': this.afAuth.auth.signInWithPopup(new firebase.auth.TwitterAuthProvider())
+      case 'twitter': this.afAuth.auth.signInWithPopup(new firebase.auth.TwitterAuthProvider()).catch(err => this.socialNetworkError(err))
         break
       case 'email_new':
         this.afAuth.auth.createUserWithEmailAndPassword(this.form.value.email, this.form.value.password)
           .then(res => {
-            console.log(res)
             res.sendEmailVerification()
-          })
+          }).catch(err => this.networkError(err))
         break
       case 'email_login':
         this.afAuth.auth.signInWithEmailAndPassword(this.form.value.email, this.form.value.password).then(res => {
           console.log(res)
         })
+          .catch(err => this.networkError(err))
         break
       default:
     }
+  }
+
+  networkError(err: any) {
+    this.form.setErrors({
+      ...this.form.errors,
+      network: err.message
+    })
+    this.cd.markForCheck()
+  }
+
+  socialNetworkError(err: any) {
+    this.socialNetworkErr = err.message
+    this.cd.markForCheck()
   }
 
   public form = new FormGroup({
