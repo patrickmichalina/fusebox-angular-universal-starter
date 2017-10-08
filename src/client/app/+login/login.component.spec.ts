01c9355b3188fa13b218f408f343ad2ad9a73885
Patrick Michalina <patrickmichalina@mac.com>
2017-10-08 10:05:19 -0500

    tests

--- src/client/app/+login/login.component.spec.ts
+++ src/client/app/+login/login.component.spec.ts
@@ -1,14 +1,22 @@
+import { AngularFireAuthModule } from 'angularfire2/auth'
 import { LoginComponent } from './login.component'
 import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 import { Component } from '@angular/core'
 import { LoginModule } from './login.module'
+import { AngularFireModule } from 'angularfire2'
 
 describe(LoginComponent.name, () => {
   let fixture: ComponentFixture<LoginComponent>
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [LoginModule],
+      imports: [LoginModule, AngularFireModule.initializeApp({
+        'apiKey': '1',
+        'authDomain': 'app.firebaseapp.com',
+        'databaseURL': 'https://app.firebaseio.com',
+        'projectId': 'firebase-app',
+        'messagingSenderId': '1'
+      }), AngularFireAuthModule],
       declarations: [TestComponent]
     }).compileComponents()
   }))
