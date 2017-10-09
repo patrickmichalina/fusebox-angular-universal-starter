c2acc67978466b40b63bf85269671ea0e24270f9
Patrick Michalina <patrickmichalina@mac.com>
2017-10-08 21:41:20 -0500

    wip

--- src/client/app/+account/account.component.ts
+++ src/client/app/+account/account.component.ts
@@ -1,10 +1,11 @@
+import { PlatformService } from './../shared/services/platform.service';
 import { Subject } from 'rxjs/Subject'
 import { TransferState } from '@angular/platform-browser'
 import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core'
 import { AngularFireAuth } from 'angularfire2/auth'
 import { AUTH_TS_KEY } from '../app.module'
 import { MatSnackBar } from '@angular/material'
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'pm-account',
@@ -14,20 +15,6 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class AccountComponent {
   @HostBinding('class.card-float-container') containerClass = true
-
-  private userSource = new Subject()
-  public user$ = this.userSource
-    .startWith(this.ts.get(AUTH_TS_KEY, {}))
-    .map(a => {
-      const emailVerified = (a as any).emailVerified
-      return {
-        ...a,
-        emailColor: emailVerified ? 'primary' : 'accent',
-        emailIcon: emailVerified ? 'fa-check-circle' : 'fa-question-circle',
-        emailTooltip: emailVerified ? 'confirmed email' : 'unconfirmed email'
-      }
-    })
-
   public form = new FormGroup({
     displayName: new FormControl('', [
       Validators.required
@@ -42,14 +29,31 @@ export class AccountComponent {
       Validators.required
     ])
   })
+  private userSource = new Subject()
+  public user$ = this.userSource
+    .startWith(this.ts.get(AUTH_TS_KEY, {}))
+    .map(a => {
+      const emailVerified = (a as any).emailVerified
+      return {
+        ...a,
+        emailColor: emailVerified ? 'primary' : 'accent',
+        emailIcon: emailVerified ? 'fa-check-circle' : 'fa-question-circle',
+        emailTooltip: emailVerified ? 'confirmed email' : 'unconfirmed email'
+      }
+    })
 
-  constructor(private afAuth: AngularFireAuth, private ts: TransferState, private snackBar: MatSnackBar) {
+  constructor(private afAuth: AngularFireAuth, private ts: TransferState, private snackBar: MatSnackBar, ps: PlatformService) {
     const authFromServer = this.ts.get(AUTH_TS_KEY, {})
     this.userSource.next(authFromServer)
     this.afAuth.idToken.subscribe(a => {
       this.userSource.next(a)
     })
-    // this.openSnackBar('asd', 'Dismiss')
+
+    if (ps.isBrowser) {
+      this.form.controls['displayName'].valueChanges.debounceTime(500).subscribe(console.log)
+      this.form.controls['phoneNumber'].valueChanges.debounceTime(500).subscribe(console.log)
+      this.form.controls['email'].valueChanges.debounceTime(500).subscribe(console.log)
+    }
   }
 
   openSnackBar(message: string, action: string) {
