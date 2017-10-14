201b4e0853133f1fdea24170b7d882291391f169
Patrick Michalina <patrickmichalina@mac.com>
2017-10-13 19:15:48 -0500

    fix: lint error (#232)

--- src/client/app/+login/login.component.spec.ts
+++ src/client/app/+login/login.component.spec.ts
@@ -1,4 +1,4 @@
-import { AuthService } from './../shared/services/auth.service';
+import { AuthService } from './../shared/services/auth.service'
 import { AngularFireAuthModule } from 'angularfire2/auth'
 import { LoginComponent } from './login.component'
 import { async, ComponentFixture, TestBed } from '@angular/core/testing'
