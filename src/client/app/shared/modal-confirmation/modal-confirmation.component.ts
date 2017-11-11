e0f930636199a4dab8214f318e552d6365797fb7
Patrick Michalina <patrickmichalina@mac.com>
2017-11-10 19:44:44 -0600

    improved page creation form

--- src/client/app/shared/modal-confirmation/modal-confirmation.component.ts
+++ src/client/app/shared/modal-confirmation/modal-confirmation.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component, Inject, Input } from '@angular/core'
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material'
+import { MAT_DIALOG_DATA } from '@angular/material'
 
 @Component({
   selector: 'pm-modal-confirmation',
@@ -11,7 +11,7 @@ export class ModalConfirmationComponent {
   @Input() message: string
   @Input() title: string
 
-  constructor(public dialog: MatDialogRef<ModalConfirmationComponent>, @Inject(MAT_DIALOG_DATA) public data: any) {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {
     if (data.message) this.message = data.message
     if (data.title) this.title = data.title
   }
