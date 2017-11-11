273fd63f5023edbc936f887482158a390f8d7f4e
Patrick Michalina <patrickmichalina@mac.com>
2017-11-10 20:14:52 -0600

    tabs

--- src/client/app/+pages/pages.component.ts
+++ src/client/app/+pages/pages.component.ts
@@ -2,6 +2,8 @@ import { FirebaseDatabaseService } from './../shared/services/firebase-database.
 import { ChangeDetectionStrategy, Component } from '@angular/core'
 import { MatDialog } from '@angular/material'
 import { PageFormComponent } from './page-form/page-form.component'
+import { ActivatedRoute, Router } from '@angular/router'
+import { Observable } from 'rxjs/Observable'
 
 @Component({
   selector: 'pm-pages',
@@ -11,6 +13,18 @@ import { PageFormComponent } from './page-form/page-form.component'
 })
 export class PagesComponent {
   pages$ = this.db.get('/pages')
+
+  private params$ = this.ar.queryParams.shareReplay()
+  private currentTab$ = this.params$
+    .map(a => a.tab ? +a.tab : 0)
+
+  view$ = Observable.combineLatest(this.currentTab$, this.pages$,
+    (currentTab, pages) => {
+      return {
+        pages,
+        currentTab
+      }
+    })
 
   ds(key: string) {
     const self = this
@@ -33,5 +47,9 @@ export class PagesComponent {
     })
   }
 
-  constructor(private db: FirebaseDatabaseService, private dialog: MatDialog) { }
+  updateTabParam(tab: number) {
+    this.router.navigate([this.router.url.split('?')[0]], { queryParams: { tab }, queryParamsHandling: 'merge' })
+  }
+
+  constructor(private db: FirebaseDatabaseService, private dialog: MatDialog, private ar: ActivatedRoute, private router: Router) { }
 }
